2ebc6c4ebb99cdbe18174faa3c9af759eaf672e5
Eric Moran <emoran0403@gmail.com>
2022-02-07 17:18:00 -0500

    function to check for numerical input is added, clearing input box function added

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 /**
- * ! need to find out how to sanitize the inputs to only accept numbers > 0 to 500, and both params under 250 for circles
  * todo - get some nice styling
  * todo - draw shapes in the shapeContainer
  * todo - Clicking on any shape should call a method named describe(), which should update the statistics in the sidepanel in index.html
@@ -8,6 +7,8 @@
  * todo - and the shape will be placed in a random location within the shape canvas
  * * - Circles should be purple, Squares should be red, Triangles should be yellow, and Rectangles should be green
  * * - clearing input after submitting => //? selector.val('') sets the value to an empty string, which will show the placeholder html text
+ * ! - need to find out how to sanitize the inputs to only accept numbers > 0 to 500, and both params under 250 for circles
+ * => only numerical input is accepted, need to further check for numbers within a range that disallows drawing a shape outside the box
  */
 
 class Shape {
@@ -91,30 +92,76 @@ let triangleButton = $(`#triangleButton`);
 let shapes = [];
 
 rectangleButton.click(() => {
-  // creates a new instance of a Rectangle
-  let newRect = new Rectangle(rectangleWidthBox.val(), rectangleHeightBox.val());
-  console.log(newRect); //* logging
-  rectangleWidthBox.val(""); // clears the inputs
-  rectangleHeightBox.val(""); // clears the inputs
+  if (isNumeric(rectangleWidthBox.val()) && isNumeric(rectangleHeightBox.val())) {
+    // creates a new instance of a Rectangle
+    let newRect = new Rectangle(rectangleWidthBox.val(), rectangleHeightBox.val());
+    console.log(newRect); //* logging
+    clearInputs();
+  } else {
+    failMessage();
+  }
 });
 
 squareButton.click(() => {
-  // creates a new instance of a Square
-  let newSquare = new Square(squareBox.val());
-  console.log(newSquare); //* logging
-  squareBox.val(""); // clears the inputs
+  if (isNumeric(squareBox.val())) {
+    // creates a new instance of a Square
+    let newSquare = new Square(squareBox.val());
+    console.log(newSquare); //* logging
+    clearInputs();
+  } else {
+    failMessage();
+  }
 });
 
 circleButton.click(() => {
-  // creates a new instance of a Circle
-  let newCircle = new Circle(circleBox.val());
-  console.log(newCircle); //* logging
-  circleBox.val(""); // clears the inputs
+  if (isNumeric(circleBox.val())) {
+    // creates a new instance of a Circle
+    let newCircle = new Circle(circleBox.val());
+    console.log(newCircle); //* logging
+    clearInputs();
+  } else {
+    failMessage();
+  }
 });
 
 triangleButton.click(() => {
   // creates a new instance of a Triangle
-  let newTriangle = new Triangle(triangleBox.val());
-  console.log(newTriangle); //* logging
-  triangleBox.val(""); // clears the inputs
+
+  if (isNumeric(triangleBox.val())) {
+    let newTriangle = new Triangle(triangleBox.val());
+    console.log(newTriangle); //* logging
+    clearInputs();
+  } else {
+    failMessage();
+  }
 });
+
+//*********************************************************************** Functions ****************************************************/
+
+function isNumeric(str) {
+  //checks if the input is a string containing a number and only a number
+  if (typeof str != "string") return false; // we only process strings!
+  return (
+    !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+    !isNaN(parseFloat(str))
+  ); // ...and ensure strings of whitespace fail
+}
+
+let failMessage = function () {
+  // this fires when the user attempts to input something nonnumerical
+  Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "Numbers only please!",
+    footer: '<a href="https://en.wikipedia.org/wiki/Number">Why do I have this issue?</a>',
+  });
+};
+
+let clearInputs = function () {
+  // clears the inputs of all boxes
+  rectangleWidthBox.val("");
+  rectangleHeightBox.val("");
+  squareBox.val("");
+  circleBox.val("");
+  triangleBox.val("");
+};
